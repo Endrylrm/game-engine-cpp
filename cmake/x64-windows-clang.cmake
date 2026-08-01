2264bd9232d4d4f4a872d60c61ede2f389eb542e
Endryl Richard Monteiro <endrylrm@hotmail.com>
2026-08-01 16:31:50 -0300

    use clang++ over clangcl.

--- cmake/x64-windows-clang.cmake
+++ cmake/x64-windows-clang.cmake
@@ -1,6 +1,3 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
-
-# Force vcpkg to use the Clang-CL toolset from Visual Studio
-set(VCPKG_PLATFORM_TOOLSET "ClangCL")

cadda40ef22a9ed983536f480d88fd0c02a42d2f
Endryl Richard Monteiro <endrylrm@hotmail.com>
2026-08-02 20:57:06 -0300

    Add a auxiliar macro for logs.

--- include/engine/core/log/Log.hpp
+++ include/engine/core/log/Log.hpp
@@ -2,15 +2,11 @@
 
 #include "engine/core/log/Logger.hpp"
 
-#define LOG_INFO(...)                                                                              \
-    Logger::Log(LogLevel::Info, __FILE_NAME__, __FUNCTION__, __LINE__, __VA_ARGS__)
-#define LOG_WARNING(...)                                                                           \
-    Logger::Log(LogLevel::Warning, __FILE_NAME__, __FUNCTION__, __LINE__, __VA_ARGS__)
-#define LOG_SUCCESS(...)                                                                           \
-    Logger::Log(LogLevel::Success, __FILE_NAME__, __FUNCTION__, __LINE__, __VA_ARGS__)
-#define LOG_ERROR(...)                                                                             \
-    Logger::Log(LogLevel::Error, __FILE_NAME__, __FUNCTION__, __LINE__, __VA_ARGS__)
-#define LOG_DEBUG(...)                                                                             \
-    Logger::Log(LogLevel::Debug, __FILE_NAME__, __FUNCTION__, __LINE__, __VA_ARGS__)
-#define LOG_TRACE(...)                                                                             \
-    Logger::Log(LogLevel::Trace, __FILE_NAME__, __FUNCTION__, __LINE__, __VA_ARGS__)
+#define LOG(level, ...) Logger::Log(level, __FILE_NAME__, __FUNCTION__, __LINE__, __VA_ARGS__)
+
+#define LOG_INFO(...) LOG(LogLevel::Info, __VA_ARGS__)
+#define LOG_WARNING(...) LOG(LogLevel::Warning, __VA_ARGS__)
+#define LOG_SUCCESS(...) LOG(LogLevel::Success, __VA_ARGS__)
+#define LOG_ERROR(...) LOG(LogLevel::Error, __VA_ARGS__)
+#define LOG_DEBUG(...) LOG(LogLevel::Debug, __VA_ARGS__)
+#define LOG_TRACE(...) LOG(LogLevel::Trace, __VA_ARGS__)

274ffb0458da5e914ab0cb27282a90c196661ba2
Endryl Richard Monteiro <endrylrm@hotmail.com>
2026-08-14 20:38:56 -0300

    make EntityAPI create entities only on the mainScene.

--- include/engine/api/EntityAPI.hpp
+++ include/engine/api/EntityAPI.hpp
@@ -15,29 +15,9 @@ public:
         return getMainScene()->createEntity();
     }
 
-    static Entity createEntityAt(std::string_view name)
-    {
-        return getActiveScene(name)->createEntity();
-    }
-
-    static Entity createEntityAt(SceneId id)
-    {
-        return getActiveScene(id)->createEntity();
-    }
-
 private:
     static Scene *getMainScene()
     {
         return getManager().getMainScene();
-    }
-
-    static Scene *getActiveScene(std::string_view name)
-    {
-        return getManager().getActiveScene(name);
-    }
-
-    static Scene *getActiveScene(SceneId id)
-    {
-        return getManager().getActiveScene(id);
     }
 };
